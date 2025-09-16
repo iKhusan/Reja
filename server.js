f2b8c735d86f0352aee9c47ae42563c7690ebda3
iKhusan <khusan10n@gmail.com>
2025-09-17 04:48:26 +0900

    done

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 console.log("web serverni boshlash");
-const express = require('express');
+const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const path = require("path");
 //1 : kirish
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 //2 Session
 //3 : views
-app.set("views", "views");
+
+
+app.set("views", __dirname);
 app.set("view engine","ejs");
+
 //4 : routing
-app.get("/hello",function(req, res) {
-    res.end(`<h1 style="background: green">Hello wrold</h1>`);
+// app.get("/hello",function(req, res) {
+//     res.end(`<h1 style="background: green">Hello wrold</h1>`);
+// });
+
+app.post("/create-item", (req, res)=>{
+    console.log(req.body);
+    res.json({test:"success"});
 });
 
+app.get("/", function(req, res){
+    res.render('harid');
+});
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
