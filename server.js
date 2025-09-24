4b76c770efbdce592ad310a4c1159d26211c3185
iKhusan <khusan10n@gmail.com>
2025-09-24 18:47:32 +0900

    connected

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require("http");
 
 const mongodb = require("mongodb");
 let db;
-const connectionString = "mongodb+srv://khusan:950870377i@cluster0.4nonkzg.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
+const connectionString = " mongodb+srv://khusan:950870377i@cluster0.4nonkzg.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
+
 
 mongodb.connect(connectionString, {useNewUrlParser: true,
     useUnifiedTopology: true,
