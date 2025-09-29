f29c0469c17e1098d0a3cd698dd8b4a4b425c86e
iKhusan <khusan10n@gmail.com>
2025-09-29 16:21:35 +0900

    Feat: MIT-C task bajarildi

--- train.js
+++ train.js
@@ -1,3 +1,79 @@
+      //MITASK-C 
+
+/****************************************************** 
+Shunday class tuzing tuzing nomi Shop,
+ va uni constructoriga 3 hil mahsulot pass bolsin, 
+hamda classning 3ta methodi bolsin, biri qoldiq, 
+biri sotish va biri qabul. 
+Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() 
+return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! 
+shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq()
+ return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+******************************************************************************/
+const moment = require("moment");
+
+class Shop {
+    constructor(Osh, Olma, Cola) {
+        this.products = {
+           Osh : Osh,
+           Olma: Olma,
+           Cola: Cola
+        };
+    }
+
+    qoldiq() {
+        const time = moment().format("HH:mm");
+        const {Osh, Olma, Cola} = this.products;
+        const text = `hozir ${time}da ${Osh}ta osh, ${Olma}kg olma va ${Cola}ta cola mavjud!`;
+        console.log(text);
+        return text;
+    }
+
+    sotish(nomi, son) {
+        const time = moment().format("HH:mm");
+        if (!Object.keys(this.products).includes(nomi)) {
+            console.log(`Mahsulot topilmadi: ${nomi}`);
+            return;
+        }
+        if (this.products[nomi] < son) {
+            console.log(`Yetarli ${nomi} yo‘q! Hozircha ${this.products[nomi]}ta bor.`);
+            return;
+        }
+        this.products[nomi] -= son;
+        console.log(`hozir ${time}da ${son}ta ${nomi} sotildi.`);
+    }
+
+    qabul(nomi, son) {
+        const time = moment().format("HH:mm");
+        if (!Object.keys(this.products).includes(nomi)) {
+            console.log(`Mahsulot topilmadi: ${nomi}`);
+            return;
+        }
+      this.products[nomi] += son;
+        console.log(`hozir ${time}da ${son}ta ${nomi} qabul qilindi.`);
+    }
+}
+
+
+const shop = new Shop(4, 5, 2);
+shop.qoldiq();            
+shop.sotish('Osh', 3);    
+shop.qabul('Cola', 4);   
+shop.qoldiq();           
+
+
+
+
+
+
+
+
+
+
+
+
+//====================================================================================================
 // TASK B: 
 
 // Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
@@ -6,24 +82,24 @@
 // Masalani yechimi:
 
 
-function countDigits(string) {
-    let count = 0; 
+// function countDigits(string) {
+//     let count = 0; 
 
 
-    for (let i = 0; i < string.length; i++) {
-        let ch = string[i]; 
+//     for (let i = 0; i < string.length; i++) {
+//         let ch = string[i]; 
 
       
-        if (ch >= '0' && ch <= '9') {
-            count = count + 1; 
-        }
-    }
+//         if (ch >= '0' && ch <= '9') {
+//             count = count + 1; 
+//         }
+//     }
 
-    return count; 
-}
+//     return count; 
+// }
 
 
-console.log(countDigits("439622igfksgfskgf7r7882")); 
+// console.log(countDigits("439622igfksgfskgf7r7882")); 
 
    
 
@@ -63,37 +139,37 @@ console.log(countDigits("439622igfksgfskgf7r7882"));
 
 
 
-console.log("Jack Ma maslahatalri");
-const list = [
-    "yaxshi talaba bo'ling", //20
-    "to'g'ri boshliq tanlang va koproq xato qling",//20-30
-    "ozingizga ishlashni boshlang",//30-40
-    "yoshlarga inves qiling",//40-50
-    "endi dam oling,foydasi yoq endi",//60
-];
+// console.log("Jack Ma maslahatalri");
+// const list = [
+//     "yaxshi talaba bo'ling", //20
+//     "to'g'ri boshliq tanlang va koproq xato qling",//20-30
+//     "ozingizga ishlashni boshlang",//30-40
+//     "yoshlarga inves qiling",//40-50
+//     "endi dam oling,foydasi yoq endi",//60
+// // ];
 
-function maslahatBering(a, callback) {
-    if(typeof a!== "number") callback ("insert a number", null);
-    else if(a <=20) callback(null, list[0]);
-    else if(a <=20) callback(null, list[1]);
-    else if(a <=20) callback(null, list[2]);
-    else if(a <=20) callback(null, list[3]);
-    else if(a <=20) callback(null, list[4]);
-    else {
-        setInterval(function () {
-            callback(null, list[4]);
-        },5000);
-    }
-}
+// function maslahatBering(a, callback) {
+//     if(typeof a!== "number") callback ("insert a number", null);
+//     else if(a <=20) callback(null, list[0]);
+//     else if(a <=20) callback(null, list[1]);
+//     else if(a <=20) callback(null, list[2]);
+//     else if(a <=20) callback(null, list[3]);
+//     else if(a <=20) callback(null, list[4]);
+//     else {
+//         setInterval(function () {
+//             callback(null, list[4]);
+//         },5000);
+//     }
+// }
 
-console.log("Passed here 0");
-maslahatBering(70, (err,data) =>{
-    if(err) console.log("ERROR:",err);
-    else{
-        console.log(data);
-    }
-});
-console.log("passed here 1");
+// console.log("Passed here 0");
+// maslahatBering(70, (err,data) =>{
+//     if(err) console.log("ERROR:",err);
+//     else{
+//         console.log(data);
+//     }
+// });
+// console.log("passed here 1");
 
 
 
